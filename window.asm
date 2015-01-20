cb6255fb3ef8f08337b6d1dfed20fcd9da169130
wuxiaojiang <717189544@qq.com>
2015-01-20 19:51:16 +0800

    Create window.asm
    窗口程序

---
+++ window.asm
@@ -0,0 +1,42 @@
+.386
+.model flat,stdcall
+option casemap:none
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+include kernel32.inc
+include user32.inc
+include windows.inc
+include gdi32.inc
+includelib kernel32.lib
+includelib user32.lib
+includelib gdi32.lib
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      .data?
+hInstance dd ?
+hWinMain dd ?
+      .const
+szClass db 'my class',0
+szCaption db 'my first window',0
+szText db 'by biopunk',0
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      .code
+_ProcWinMain proc use ebx edi esi,hWnd,uMsg,wParam,lParam
+             local @stPs:PAINTSTRUCT
+             local @stRect:RECT
+             local @hDc
+            
+             mov eax,uMsg
+;************************************************************
+            .if eax == WM_PAINT
+                invoke BeginPaint,hWnd,addr @stPs
+                mov @hDc,eax
+            
+                invoke GetClientRect,hWnd,addr @stRect
+                invoke DrawText,@hDc,addr szText,-1,addr @stRect,DT_SINGLELINE or DT_CENTER or DT_VCENTER
+                invoke Endpaint,hWnd,addr @stPs
+            
+            .elseif eax == WM_CLOSE
+                    invoke 
+             
+             
+             
+            
