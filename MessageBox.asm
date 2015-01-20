c5b689703d68a3aee207a97c7655eed196caf70e
wuxiaojiang <717189544@qq.com>
2015-01-20 11:59:05 +0800

    Create MessageBox.asm
    win32汇编写的messagebox程序

---
+++ MessageBox.asm
@@ -0,0 +1,20 @@
+.386
+.model flat,stdcall
+option casemap:none
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+include windows.inc
+include user32.inc
+include kernel32.inc
+includelib user32.lib
+includelib kernel32.lib
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      .data
+szCaption db 'MessageBox!',0
+szText db 'Hello world!',0
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      .code
+start:
+      invoke MessageBox,NULL,offset szText,offset szCaption,MB_OK
+      invoke ExitProcess,NULL
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+end start
