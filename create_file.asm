d2c84ec8ff3bcfd386e3bf28e099532080e0a982
wuxiaojiang <717189544@qq.com>
2015-02-09 13:28:50 +0800

    Update create_file.asm

--- create_file.asm
+++ create_file.asm
@@ -1,1 +1,18 @@
 .386
+.model flat,stdcall
+option casemap:none
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+include    windows.inc
+include    user32.inc
+include    kernel32.inc
+include    comdlg32.inc
+includelib user32.lib
+includelib kernel32.lib
+includelib comdlg32.lib
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ICO_MAIN    equ     1000
+DLG_MAIN    equ     100
+IDC_FILE    equ     101
+IDC_BROWSE  equ     102
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	.data?
